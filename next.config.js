9ae273179ca25bee33cdaac3b5e80c893e7541c9
Pawan608 <agarwalpawan6089@gmail.com>
2023-12-10 18:08:21 +0530

    server

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  server: {
+    host: "0.0.0.0",
+  },
   images: {
-    domains: ['firebasestorage.googleapis.com'],
+    domains: ["firebasestorage.googleapis.com"],
   },
 };
 
