6c04c1775dad6fea5a7f5cf6679c5d152a97570f
Pawan608 <agarwalpawan6089@gmail.com>
2024-01-15 22:35:04 +0530

    new

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   server: {
     host: "0.0.0.0",
-    port: 3004,
+    port: 80,
   },
   images: {
     domains: ["firebasestorage.googleapis.com"],
@@ -12,7 +12,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
- 
 };
 
 module.exports = nextConfig;
